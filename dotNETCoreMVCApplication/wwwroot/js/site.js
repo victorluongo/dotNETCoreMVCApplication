b7a95f74d92b62683d99fd78df4a7e05b9c894b2
Victor Luongo <victor.luongo@gmail.com>
2022-06-07 11:05:57 -0300

    ZipCode Search (Supplier)

--- dotNETCoreMVCApplication/wwwroot/js/site.js
+++ dotNETCoreMVCApplication/wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function ZipCodeSearch() {
+    $(document).ready(function () {
+
+        function clear_zipcode_form() {
+            $("#Address_StreetName").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        $("#Address_ZipCode").blur(function () {
+
+            var zipcode = $(this).val().replace(/\D/g, '');
+
+            if (zipcode != "") {
+
+                var zipcodeValidation = /^[0-9]{8}$/;
+
+                if (zipcodeValidation.test(zipcode)) {
+
+                    $("#Address_StreetName").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + zipcode + "/json/?callback=?",
+                        function (data) {
+                            if (!("erro" in data)) {
+                                $("#Address_StreetName").val(data.logradouro);
+                                $("#Address_City").val(data.localidade);
+                                $("#Address_State").val(data.uf);
+                            } else {
+                                clear_zipcode_form();
+                                alert("ZipCode not found.");
+                            }
+                        });
+                } else {
+                    clear_zipcode_form();
+                    alert("Invalid ZipCode format.");
+                }
+            } else {
+                clear_zipcode_form();
+            }
+        });
+    });
+}
